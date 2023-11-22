e54b71dd2cdc65f90e988de80630d6da432bbbd9
leroyloic <loic.mewoulou@cm.maviance.com>
2023-11-22 06:17:55 +0100

    updated package.json and created 1st endpoints

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./src/routes/crmroutes";
+
+const app = express();
+const PORT = 3000;
+
+routes(app);
+app.get("/", (req, res) => {
+    res.send("node and express are running on port " + PORT);
+    });         
+
+app.listen(PORT, () => {
+    console.log("your server is running on port " + PORT);
+});
